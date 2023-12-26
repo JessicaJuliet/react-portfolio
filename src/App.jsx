c9eee6824c7af02edf6dbffe918b496e906e612b
Jessica <72465125+JessicaJuliet@users.noreply.github.com>
2023-12-26 20:08:14 +0000

    refactor(Navbar): Add navbar, link data & styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+  return <>
+  <Navbar />
+  </>
 }
+
+export default App
---
+++ src/data.jsx
@@ -0,0 +1,9 @@
+import {nanoid} from "nanoid";
+import {FaHtml5, FaJs, FaReact} from "react-icons/fa";
+
+export const links = [
+    {id: nanoid(), href: "#home", text:"Home"},
+    {id: nanoid(), href: "#about", text:"About"},
+    {id: nanoid(), href: "#skills", text:"Skills"},
+    {id: nanoid(), href: "#my-work", text:"My Work"},
+];
