1a04e4c6bc784763c71c6e9370ea9218b4cc3d7b
Jessica <72465125+JessicaJuliet@users.noreply.github.com>
2023-12-27 15:18:10 +0000

    style(index): create custome align-element class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {links} from "../data";
 const Navbar = () => {
   return <>
     <nav className="bg-emerald-100">
-        <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row
+        <div className="align-element py-4 flex flex-col sm:flex-row
         sm:gap-x-16 sm:items-center sm:py-8">
             <h2 className="text-3xl font-bold">Web<span className="text-emerald-600">Dev</span></h2>
             <div className="flex gap-x-3">
