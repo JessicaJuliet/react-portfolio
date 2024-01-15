17115c0b2b8e2795fcfea53d7e280dd0f97c54b7
Jessica <72465125+JessicaJuliet@users.noreply.github.com>
2024-01-15 16:21:26 +0000

    refactor(ProjectsCard): add right arrow icon

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,5 +1,6 @@
 import {FaGithubSquare} from "react-icons/fa";
 import {TbWorldWww} from "react-icons/tb";
+import {FaArrowRight} from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const ProjectsCard = ({url, img, pageLink, github, title, technology, text}) => {
@@ -9,8 +10,11 @@ const ProjectsCard = ({url, img, pageLink, github, title, technology, text}) => 
             <img src={img} alt={title} className="w-full object-cover rounded-t-lg h-64"/>
             <div className="p-8">
                 <h2 className="text-xl tracking-wide capitalize font-medium">{title}</h2>
-                <p className="mt-4 text-slate-700 capitalize leading-loose font-light"><b>Technologies:</b> {technology}</p>
-                <p className="mt-4 text-slate-700 leading-loose">{text}</p>
+                <p className="my-4 text-slate-700 capitalize leading-loose font-light"><b>Technologies:</b> {technology}</p>
+                <p className="my-4 text-slate-700 leading-loose">{text}</p>
+                <div className="flex justify-end">
+                    <FaArrowRight className="h-5 w-5 text-slate-900 hover:text-slate-500 duration-300" />
+                </div>
                 {/* <div className="mt-4 flex gap-x-4">
                     <a href={url}>
                         <TbWorldWww className="h-8 w-8 text-slate-500 hover:text-slate-950 duration-300" />
