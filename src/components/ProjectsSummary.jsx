fa1d1cde2be7a52fc081384943a7f5b1cf418bae
Jessica <72465125+JessicaJuliet@users.noreply.github.com>
2024-01-29 16:41:07 +0000

    refactor(ProjectsSummary): remove commented out code

--- src/components/ProjectsSummary.jsx
+++ src/components/ProjectsSummary.jsx
@@ -1,6 +1,3 @@
-import { MdDateRange } from "react-icons/md";
-
-
 
 const ProjectsSummary = ({industry, date, tech}) => {
   return (
@@ -19,20 +16,3 @@ const ProjectsSummary = ({industry, date, tech}) => {
 }
 
 export default ProjectsSummary;
-
-
-// const ProjectsSummary = ({industry, date, tech}) => {
-//   return (
-//     <div className="py-5 text-slate-700 leading-loose">
-//       <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
-//         <b>Industry:</b> {industry}
-//       </p>
-//       <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
-//         <b>Date:</b> {date}
-//       </p>
-//       <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
-//         <b>Technologies:</b> {tech}
-//       </p>
-//     </div>
-//   )
-// }
