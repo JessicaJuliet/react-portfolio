911370e34392a3dbd22331bac75f90a0bd835897
Jessica <72465125+JessicaJuliet@users.noreply.github.com>
2024-01-29 15:00:13 +0000

    refactor(shopifyBlogPage): change layout, styles and add images

--- src/components/ProjectsSummary.jsx
+++ src/components/ProjectsSummary.jsx
@@ -5,13 +5,13 @@ import { MdDateRange } from "react-icons/md";
 const ProjectsSummary = ({industry, date, tech}) => {
   return (
     <div className="py-5 text-slate-700 leading-loose">
-      <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
+      <p className="py-1 md:grid md:grid-cols-[25%_minmax(auto,_70%)] gap-5">
         <b>Industry:</b> {industry}
       </p>
-      <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
+      <p className="py-1 md:grid md:grid-cols-[25%_minmax(auto,_70%)] gap-5">
         <b>Date:</b> {date}
       </p>
-      <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
+      <p className="py-1 md:grid md:grid-cols-[25%_minmax(auto,_70%)] gap-5">
         <b>Technologies:</b> {tech}
       </p>
     </div>
@@ -19,3 +19,20 @@ const ProjectsSummary = ({industry, date, tech}) => {
 }
 
 export default ProjectsSummary;
+
+
+// const ProjectsSummary = ({industry, date, tech}) => {
+//   return (
+//     <div className="py-5 text-slate-700 leading-loose">
+//       <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
+//         <b>Industry:</b> {industry}
+//       </p>
+//       <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
+//         <b>Date:</b> {date}
+//       </p>
+//       <p className="py-1 gap-2 grid grid-cols-1 md:grid-cols-[15%_minmax(auto,_40%)]">
+//         <b>Technologies:</b> {tech}
+//       </p>
+//     </div>
+//   )
+// }
--- src/pages/shopifyBlogPage.jsx
+++ src/pages/shopifyBlogPage.jsx
@@ -2,11 +2,15 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import SectionTitle from "../components/SectionTitle";
 import SectionSubtitle from "../components/SectionSubtitle";
-import ProjectsSummary from '../components/projectsSummary';
+import ProjectsSummary from "../components/projectsSummary";
 
-
-// Media imports (img/mp4)
+// Media imports
 import blogPage from "../assets/blogMedia/blogPage.png";
+import blogEngagement from "../assets/blogMedia/blog-engagement.png";
+import blogMenu from "../assets/blogMedia/blog-menu.png";
+import blogEducation from "../assets/blogMedia/blog-education.png";
+import blogArticle from "../assets/blogMedia/blog-article.png";
+import blogContent from "../assets/blogMedia/blog-content.png";
 
 
 
@@ -14,44 +18,61 @@ const shopifyBlogPage = () => {
   return (
     <>
     <Navbar />
-      <section className='bg-white'>
-        <div className='align-element py-16'>
-          <SectionTitle text="Shopify Blog" />
-          <ProjectsSummary industry="Luxury Jewellery" date="2023" tech="HTML, CSS, Vanilla JavaScript, SCSS, Shopify, Liquid, Matrixify app, Git" />
+      <section className='bg-white text-slate-600'>
 
-          <div className="grid md:grid-cols-2 md:gap-16 text-slate-600">
-            <article>
-              <SectionSubtitle text="Requirements" />
-              <p className='mt-2'>
-                To develop a blog on Shopify, migrating the old WordPress blog, in order to consolidate all content under one platform to streamline management processes for the tech team.
-              </p>
-              <SectionSubtitle text="Approach" />
-              <p className='mt-2'>
-                Prior to commencing any development work I consulted with the Marketing team as the migration would directly impact their processes. I outlined the pros and cons of migrating the blog and the reasons why tech were considering this.
-              </p>
-              <p className='mt-2'>
-                Following the relevant stakeholder buyin, I approached the WordPress blog review methodically and commenced the development work. I developed a fully responsive choronological blog on Shopify which allows users to filter by post category.
-              </p>
-            </article>
-
-            <article>
-              <SectionSubtitle text="Challenges & Outcome" />
-              <p className='mt-2'>
-                A key component of this project was reviewing a considerable amount of older blog posts, images and metadata on WordPress to ensure their relevancy before exporting and migrating them to Shopify. The marketing team did not have the capcity to take on this project and as a result it was delayed.
-              </p>
-              <p className='mt-2'>
-                I took the initiative to carry out this review as it was in our interest to complete this ahead of a major rebrand launch in 2023. My familiarity with WordPress, SEO and marketing aided me in this process and subsequently in the successful launch of the new Shopify blog.
-              </p>
-            </article>
+        <div className='px-5 md:pt-20 md:px-16 border-b-2 border-b-black-900'>
+          <div className="grid m-auto md:grid-cols-2 md:max-w-screen-lg md:gap-36">
+            <div className='pt-12'>
+              <SectionTitle text="Shopify Blog" />
+              <ProjectsSummary industry="Luxury Jewellery" date="2023" tech="HTML, CSS, Vanilla JavaScript, SCSS, Shopify, Liquid, Matrixify app, Git" />
+            </div>
+            <div>
+              <img className="w-96 h-96 object-cover object-top" src={blogEngagement} />
+            </div>
           </div>
+        </div>
 
-          <SectionSubtitle text="Images" />
-          <div className="grid gap-10 grid-cols-1 py-10">
-            <img src={blogPage} alt='Shopify blog page' className='shadow-md max-w-3xl'/>
+        <div className="px-5 py-10 grid grid-cols-1 gap-5 md:gap-10 max-w-full md:py-20 bg-slate-50 border-b-2 border-b-black-900">
+          <img src={blogPage} alt='Shopify blog page' className='shadow-md m-auto md:max-w-screen-lg'/>
+          <article className="m-auto md:max-w-2xl leading-loose">
+            <SectionSubtitle text="Requirements" />
+            <p className='mt-2'>
+              To develop a blog on Shopify, migrating the old WordPress blog, in order to consolidate all content under one platform to streamline management processes for the tech team.
+            </p>
+          </article>
+        </div>
+
+        <div className="px-5 py-10 grid grid-cols-1 gap-5 md:gap-10 max-w-full max-px-16 md:py-20 border-b-2 border-b-black-900">
+          <article className="m-auto md:max-w-2xl leading-loose">
+            <SectionSubtitle text="Approach" />
+            <p className='mt-2'>
+              Prior to commencing any development work I consulted with the Marketing team as the migration would directly impact their processes. I outlined the pros and cons of migrating the blog and the reasons why tech were considering this.
+            </p>
+            <p className='mt-2'>
+              Following the relevant stakeholder buyin, I approached the WordPress blog review methodically and commenced the development work. I developed a fully responsive choronological blog on Shopify which allows users to filter by post category.
+            </p>
+          </article>
+
+          <article className="m-auto md:max-w-2xl leading-loose">
+            <SectionSubtitle text="Challenges & Outcome" />
+            <p className='mt-2'>
+              A key component of this project was reviewing a considerable amount of older blog posts, images and metadata on WordPress to ensure their relevancy before exporting and migrating them to Shopify. The marketing team did not have the capcity to take on this project and as a result it was delayed.
+            </p>
+            <p className='mt-2'>
+              I took the initiative to carry out this review as it was in our interest to complete this ahead of a major rebrand launch in 2023. My familiarity with WordPress, SEO and marketing aided me in this process and subsequently in the successful launch of the new Shopify blog.
+            </p>
+          </article>
+
+          <div className="m-auto grid gap-5 sm:grid-cols-2 md:grid-cols-4 md:max-w-screen-lg">
+            <img src={blogMenu} alt="Blog menu" />
+            <img src={blogEducation} alt="Blog education page" />
+            <img src={blogArticle} alt="Blog article" />
+            <img src={blogContent} alt="Blog content" />
           </div>
 
         </div>
       </section>
+
       <Footer />
     </>
   )
